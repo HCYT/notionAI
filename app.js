0cf97e3dc609c11b0658cfd5cdf05ac5d51055a1
cyh <cyh@hcytlog.com>
2023-03-04 22:50:31 +0800

    feat:add annotation

--- app.js
+++ app.js
@@ -23,16 +23,16 @@ let result = "";
 //     console.error(err);
 // });
 
-// let pageTitle = "Scrum 團隊成員組成介紹"
-// let pageContent = "這份文件介紹了 Scrum 團隊成員的組成，包括 Scrum Master、產品負責人和開發團隊成員"
-// let selectedText = ""
-// let prompt="寫一手情詩";
-// await notionAI.writingPrompt(prompt, pageTitle, selectedText, pageContent).then((text) => {
-//     result = text;
-//     console.log(`Writing with topic "${topic}": ${result}`);
-// }).catch((err) => {
-//     console.error(err);
-// });
+let pageTitle = "Scrum 團隊成員組成介紹"
+let pageContent = "這份文件介紹了 Scrum 團隊成員的組成，包括 Scrum Master、產品負責人和開發團隊成員"
+let selectedText = ""
+let prompt="寫一手情詩";
+await notionAI.writingPrompt(prompt, pageTitle, selectedText, pageContent).then((text) => {
+    result = text;
+    console.log(`Writing with topic "${topic}": ${result}`);
+}).catch((err) => {
+    console.error(err);
+});
 
 
 // let text = "This document provides an introduction to the composition of a Scrum team, including the Scrum Master, the Product Owner, and the development team members."
