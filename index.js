7d235f99d745ca345959ce2f0d0233b0a1c0fce3
cyh <cyh@hcytlog.com>
2023-03-06 15:14:14 +0800

    feat:fix npm error

---
+++ index.js
@@ -0,0 +1,8 @@
+const { NotionAI } = require('./src/notionAI');
+const { TOPICS, TONE, LANGUAGE } = require('./src/types.js')
+module.exports = {
+    NotionAI,
+    TOPICS,
+    TONE,
+    LANGUAGE
+}
